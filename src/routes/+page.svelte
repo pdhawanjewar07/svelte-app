b32491bf680a81cc958b378726144611ce5a88e3
PRATHAMESH DHAWANJEWAR <pdhawanjewar07@gmail.com>
2023-09-29 09:34:41 +0530

    initial commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,15 @@
-
+<script>
+    let count = 0;
+</script>
 
 <h1>Main page</h1>
 <button><a href="./about">About</a></button>
+
+<div>
+    <p>Counter : {count}</p>
+    <button on:click={()=> count+=1}>Increase</button>
+    <button on:click={()=> count-=1}>Decrease</button>
+</div>
 
 <style>
     h1{
