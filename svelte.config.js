3e649ccc686bba3bbc33c9830d830217d9a52c51
PRATHAMESH DHAWANJEWAR <pdhawanjewar07@gmail.com>
2023-09-29 09:04:55 +0530

    initial commit

--- svelte.config.js
+++ svelte.config.js
@@ -15,7 +15,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		paths: {
-			base: dev ? '' : process.env.BASE_PATH,
+			base: dev ? 'https://pdhawanjewar07.io/svelte-app' : process.env.BASE_PATH,
 		}
 	}
 };
